eb40ecf702a90ba024e84eee81f46b8e5c622f22
Gvitor10 <gvitor7062@gmail.com>
2022-07-21 21:10:37 -0300

    Resolvido os exercicios 1,2 e 3

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+// Exercicio 01
+
+let numeroPar = Number(prompt("Insira um numero par:"));
+
+console.log("O que sobra dividindo o seu numero por 2:", numeroPar % 2);
+
+// C- Foi observado que qualquer numero par que é inserido, o resto sempre será 0.
+
+// D- Foi observado que qualquer numero impar que é inserido, o resto sempre será 1.
---
+++ exercicio2/script.js
@@ -0,0 +1,13 @@
+// Exercicio 02
+
+const num1 = Number(prompt("Insira aqui o primeiro numero:"));
+
+const num2 = Number(prompt("Insira aqui o segundo numero:"));
+
+console.log("O primeiro numero é maior que o segundo? - ", num1 > num2);
+
+console.log("O primeiro numero é igual ao segundo? - ", num1 == num2);
+
+console.log("O primeiro numero é divisivel pelo segundo? - ", (num1 % num2) == 0);
+
+console.log("O segundo numero é divisivel pelo primeiro numero? - ", (num2 % num1) == 0);
